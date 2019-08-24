23d94afa0dbe1399ab67e28c394edf94384b4050
Juan Pablo Armenta <juanpabload98@gmail.com>
2019-08-23 23:11:49 -0600

    tercer commit

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ app.set("port",4000);
 app.set("views",path.join(__dirname,"views"));
 app.set("view engine","ejs");
 //middleware
+//archivos estaticos
+app.use(express.static(path.join(__dirname,"public")));
 //rutas
 app.use(require("./routes/index.js"));
 //archivos estaticos
